5ff26f4e254ff83fad04e64b91721a8ff6412d80
Gerrit Garbereder <info@garbereder.com>
2023-06-08 21:46:46 +0300

    Add first version of cypress tests

---
+++ cypress/e2e/Index/simple.cy.js
@@ -0,0 +1,40 @@
+/// <reference types="cypress" />
+
+describe('Garbereder.com', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:8080')
+  })
+
+  it('Shows the portait', () => {
+    cy.get('img').should('have.attr', 'src').should('include', 'portrait')
+  })
+
+  it('Shows the name', () => {
+    cy.get('.text-orange-600').should('have.text', 'Gerrit Garbereder')
+  })
+
+  it('Shows the resume link', () => {
+    const link = cy.get('a').first()
+    link.should('have.text', 'Resume')
+    link.should('have.attr', 'href').should('eq', '#cv')
+  })
+
+  it('Shows the resume', () => {
+    cy.get('ol').find('li').should('have.length', 5)
+    cy.get('ol').find('li').find('.job-date')
+      .should('exist')
+      .invoke('text')
+      .should('match', /20[0-9]{2}\s-\sPresent|(20[0-9]{2})/)
+    cy.get('ol').find('li').find('.job-title').should('exist')
+    cy.get('ol').find('li').find('.job-description').should('exist')
+  })
+
+  it('Shows the email', () => {
+    cy.get('a[href*="mailto:info@garbereder.com"]').should('exist')
+  })
+
+  it('Shows the phonenumber', () => {
+    cy.get('a[href*="tel:+37069456497"]').should('exist')
+  })
+
+})
