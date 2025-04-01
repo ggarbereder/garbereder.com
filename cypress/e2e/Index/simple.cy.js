eae76f255c3750a865ec09f7e78fd3f635240584
Gerrit <g.garbereder@gmail.com>
2025-04-01 09:25:18 +0300

    Fa removal (#139)
    * Add serve and watch scripts
    
    * Remove font awesome dependency

--- cypress/e2e/Index/simple.cy.js
+++ cypress/e2e/Index/simple.cy.js
@@ -42,4 +42,24 @@ describe(`Landingpage`, () => {
     cy.get('a[href*="tel:+37069456497"]').should('exist')
   })
 
+  it('Shows linkedin link with icon', () => {
+    cy.get('a[href*="https://www.linkedin.com/in/gerrit-garbereder-a74b6a96/"]').should('exist')
+  })
+
+  it('Shows github link with icon', () => {
+    cy.get('a[href*="https://github.com/ggarbereder/garbereder.com"]').should('exist')
+  })
+
+  it('Shows icons8 link', () => {
+    cy.get('a[href*="//icons8.com"]').should('exist')
+  })
+
+  it('Shows icons8 linkedin link', () => {
+    cy.get('a[href*="//icons8.com/icon/84888/linkedin"]').should('exist')
+  })
+
+  it('Shows icons8 github link', () => {
+    cy.get('a[href*="//icons8.com/icon/20675/github"]').should('exist')
+  })
+
 })
