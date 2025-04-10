4894872aa150be75c2020217bceb5935439721a8
Gerrit Garbereder <gerrit@garbereder.com>
2025-04-10 06:57:57 +0300

    Update tailwind to use ESM

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-module.exports = {
-    theme: {
-      extend: {},
-      fontFamily: {
-        'sans': ['Quicksand', 'ui-sans-serif', 'system-ui']
-      }
-    },
-    variants: {},
-    plugins: [],
-    content: ["./src/**/*.{html,js}"]
-  }
+export default {
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans': ['Quicksand', 'ui-sans-serif', 'system-ui']
+    }
+  },
+  variants: {},
+  plugins: [],
+  content: ["./src/**/*.{html,js}"]
+}
