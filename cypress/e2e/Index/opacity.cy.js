1987586ffa97a5d440f96d61eb2a967683b5c076
Gerrit Garbereder <gerrit@garbereder.com>
2025-04-12 08:28:29 +0300

    Use prettier for astro

--- cypress/e2e/Index/opacity.cy.js
+++ cypress/e2e/Index/opacity.cy.js
@@ -2,41 +2,41 @@
 
 describe('Opacity Observer', () => {
   beforeEach(() => {
-    cy.visit('/')
-  })
+    cy.visit('/');
+  });
 
   it('should have hidden elements initially', () => {
     // Elements should start with opacity-0 animate class
-    cy.get('.job').should('have.class', 'opacity-0')
-    cy.get('.job').should('have.class', 'animate')
-  })
+    cy.get('.job').should('have.class', 'opacity-0');
+    cy.get('.job').should('have.class', 'animate');
+  });
 
   it('should add opacity-100 class when elements are scrolled into view', () => {
     // Scroll to CV section
-    cy.get('#cv').scrollIntoView()
+    cy.get('#cv').scrollIntoView();
 
     // First job entry should become visible
-    cy.get('.job').first().should('have.class', 'opacity-100')
-  })
+    cy.get('.job').first().should('have.class', 'opacity-100');
+  });
 
   it('should reveal all job entries when scrolling to bottom', () => {
     // Scroll to bottom of the page
-    cy.scrollTo('bottom')
+    cy.scrollTo('bottom');
 
     // All job entries should become visible
     cy.get('.job').each(($el) => {
-      cy.wrap($el).should('have.class', 'opacity-100')
-    })
-  })
+      cy.wrap($el).should('have.class', 'opacity-100');
+    });
+  });
 
   it('should maintain visibility after elements are revealed', () => {
     // Scroll to bottom
-    cy.scrollTo('bottom')
+    cy.scrollTo('bottom');
 
     // Scroll back to top
-    cy.scrollTo('top')
+    cy.scrollTo('top');
 
     // Elements should remain visible
-    cy.get('.job').should('have.class', 'opacity-100')
-  })
-})
+    cy.get('.job').should('have.class', 'opacity-100');
+  });
+});
--- cypress/e2e/Index/simple.cy.js
+++ cypress/e2e/Index/simple.cy.js
@@ -2,8 +2,8 @@
 
 describe('Landing Page', () => {
   beforeEach(() => {
-    cy.visit('/')
-  })
+    cy.visit('/');
+  });
 
   describe('Header Section', () => {
     it('displays header content correctly', () => {
@@ -11,70 +11,70 @@ describe('Landing Page', () => {
       cy.get('img#portrait')
         .should('be.visible')
         .and('have.attr', 'src')
-        .and('not.be.empty')
+        .and('not.be.empty');
 
       // Name and title
-      cy.get('h1').should('contain.text', 'Welcome')
-      cy.get('.text-orange-500').should('contain.text', 'Gerrit Garbereder')
+      cy.get('h1').should('contain.text', 'Welcome');
+      cy.get('.text-orange-500').should('contain.text', 'Gerrit Garbereder');
 
       // Description
       cy.get('header p')
         .should('exist')
-        .and('contain.text', 'Senior Engineering Manager')
+        .and('contain.text', 'Senior Engineering Manager');
 
       // Resume link
       cy.get('header nav a')
         .should('have.text', 'Resume')
         .and('have.attr', 'href', '#cv')
-        .and('have.class', 'bg-orange-500')
-    })
-  })
+        .and('have.class', 'bg-orange-500');
+    });
+  });
 
   describe('Resume Section', () => {
     it('displays correct number of job entries', () => {
-      cy.get('#cv ol li').should('have.length', 6)
-    })
+      cy.get('#cv ol li').should('have.length', 6);
+    });
 
     it('validates job entry structure', () => {
       cy.get('#cv ol li').each(($li) => {
         // Check entry structure
-        cy.wrap($li).find('.bullet').should('exist')
+        cy.wrap($li).find('.bullet').should('exist');
         cy.wrap($li)
           .find('.job-date')
           .invoke('text')
-          .should('match', /^(20[0-9]{2}\s[–]\s(Present|20[0-9]{2}))$/) // Updated regex to handle both - and –
-        cy.wrap($li).find('.job-title').invoke('text').should('not.be.empty')
+          .should('match', /^(20[0-9]{2}\s[–]\s(Present|20[0-9]{2}))$/); // Updated regex to handle both - and –
+        cy.wrap($li).find('.job-title').invoke('text').should('not.be.empty');
         cy.wrap($li)
           .find('.job-description p')
-          .should('have.length.at.least', 1)
-      })
-    })
+          .should('have.length.at.least', 1);
+      });
+    });
 
     it('verifies chronological order', () => {
-      const dates = []
+      const dates = [];
       cy.get('#cv ol li .job-date')
         .each(($date) => {
-          dates.push($date.text().split(' -')[0])
+          dates.push($date.text().split(' -')[0]);
         })
         .then(() => {
-          const sortedDates = [...dates].sort((a, b) => b - a)
-          expect(dates).to.deep.equal(sortedDates)
-        })
-    })
-  })
+          const sortedDates = [...dates].sort((a, b) => b - a);
+          expect(dates).to.deep.equal(sortedDates);
+        });
+    });
+  });
 
   describe('Contact Section', () => {
     it('displays contact information correctly', () => {
       // Email
       cy.get('a[href*="mailto:gerrit@garbereder.com"]')
         .should('be.visible')
-        .and('contain.text', 'Gerrit@Garbereder.com')
+        .and('contain.text', 'Gerrit@Garbereder.com');
 
       // Phone
       cy.get('a[href*="tel:+37069456497"]')
         .should('be.visible')
-        .and('contain.text', '+370 6 945 6497')
-    })
+        .and('contain.text', '+370 6 945 6497');
+    });
 
     it('displays social links correctly', () => {
       // LinkedIn
@@ -86,7 +86,7 @@ describe('Landing Page', () => {
           'https://www.linkedin.com/in/gerrit-garbereder-a74b6a96/'
         )
         .find('svg')
-        .should('exist')
+        .should('exist');
 
       // GitHub
       cy.get('a[href*="github.com/ggarbereder"]')
@@ -97,29 +97,29 @@ describe('Landing Page', () => {
           'https://github.com/ggarbereder/garbereder.com'
         )
         .find('svg')
-        .should('exist')
-    })
+        .should('exist');
+    });
 
     it('displays attribution links correctly', () => {
       // Icons8 links
-      cy.get('footer div.text-xs a').should('have.length', 3)
-      cy.get('a[href*="icons8.com/icon/84888/linkedin"]').should('exist')
-      cy.get('a[href*="icons8.com/icon/20675/github"]').should('exist')
-      cy.get('a[href="//icons8.com"]').should('exist')
-    })
-  })
+      cy.get('footer div.text-xs a').should('have.length', 3);
+      cy.get('a[href*="icons8.com/icon/84888/linkedin"]').should('exist');
+      cy.get('a[href*="icons8.com/icon/20675/github"]').should('exist');
+      cy.get('a[href="//icons8.com"]').should('exist');
+    });
+  });
 
   describe('Accessibility and SEO', () => {
     it('has proper meta tags', () => {
-      cy.get('head meta[name="description"]').should('exist')
-      cy.get('head meta[property="og:title"]').should('exist')
-      cy.get('head meta[property="og:type"]').should('exist')
-      cy.get('head meta[property="og:url"]').should('exist')
-    })
+      cy.get('head meta[name="description"]').should('exist');
+      cy.get('head meta[property="og:title"]').should('exist');
+      cy.get('head meta[property="og:type"]').should('exist');
+      cy.get('head meta[property="og:url"]').should('exist');
+    });
 
     it('has proper document structure', () => {
-      cy.get('html').should('have.attr', 'lang', 'en')
-      cy.get('title').should('not.be.empty')
-    })
-  })
-})
+      cy.get('html').should('have.attr', 'lang', 'en');
+      cy.get('title').should('not.be.empty');
+    });
+  });
+});
