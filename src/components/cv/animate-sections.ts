ba7bb0527c5aaf27297fe066ec7290daafa53edf
Gerrit Garbereder <gerrit@garbereder.com>
2025-04-12 07:49:43 +0300

    Move ts files closer to the component

---
+++ src/components/cv/animate-sections.ts
@@ -0,0 +1,23 @@
+class OpacityObserver {
+  observer: IntersectionObserver;
+
+  constructor(cls: string) {
+    this.observer = new IntersectionObserver(entries => {
+      entries.forEach(e => {
+        if (e.isIntersecting) {
+          e.target.classList.add(cls)
+        }
+      })
+    });
+  }
+
+  observe(element: Element, selector: string) {
+    element
+      .querySelectorAll(selector)
+      .forEach(e => this.observer.observe(e))
+  }
+}
+
+document.querySelectorAll<HTMLElement>('[data-animate-section]').forEach(section => {
+  new OpacityObserver("opacity-100").observe(section, '.opacity-0.animate');
+});
