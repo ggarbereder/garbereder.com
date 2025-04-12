072b45016675d3a529e0433abd907290f62b5c2a
Gerrit Garbereder <gerrit@garbereder.com>
2025-04-12 07:49:43 +0300

    Move ts files closer to the component

--- src/lib/animate-sections.ts
+++
@@ -1,5 +0,0 @@
-import { OpacityObserver } from "./index";
-
-document.querySelectorAll<HTMLElement>('[data-animate-section]').forEach(section => {
-  new OpacityObserver("opacity-100").observe(section, '.opacity-0.animate');
-});
