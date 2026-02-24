22dbc93758525e7d54aabbf6c8f8bac44e8aaadd
Gerrit Garbereder <gerrit.garbereder@nordsec.com>
2026-02-24 11:10:25 +0200

    Improve loading

--- src/components/portrait.jsx
+++ src/components/portrait.jsx
@@ -6,7 +6,8 @@ export default function Portrait() {
       src={images[0]}
       srcset={`${images[0]} 1x, ${images[1]} 2x, ${images[2]} 3x`}
       alt="Portrait of Gerrit Garbereder"
-      decoding="async"
+      loading="eager"
+      fetchPriority="high"
       class="rounded-full border-slate-300 border-8 w-64 lg:w-64 xl:w-80 2xl:w-[400px] h-64 lg:h-64 xl:h-80 2xl:h-[400px] shadow-2xl transition-all duration-300 hover:scale-105 hover:border-orange-500 hover:shadow-orange-200"
       id="portrait"
     />
